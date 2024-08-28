87f8d5c229b39b0e4a19d558455e87ae525f3ced
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-08-29 06:42:29 +1200

    adjusted rendering order.

--- src/main/java/com/mclegoman/mclmaf2025/mixin/client/Sky/WorldRendererMixin.java
+++ src/main/java/com/mclegoman/mclmaf2025/mixin/client/Sky/WorldRendererMixin.java
@@ -24,7 +24,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
 	@Shadow @Nullable private ClientWorld world;
-	@Inject(method = "renderSky(Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getStarBrightness(F)F"))
+	@Inject(method = "renderSky(Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderColor(FFFF)V", ordinal = 4))
 	private void mclmaf2025$renderObjects(Matrix4f modelViewMatrix, Matrix4f projectionMatrix, float tickDelta, Camera preStep, boolean skipRendering, Runnable preRender, CallbackInfo ci) {
 		if (world != null) {
 			MatrixStack matrices = new MatrixStack();
