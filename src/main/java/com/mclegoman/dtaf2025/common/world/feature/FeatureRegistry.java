62ddf1089bc12d36abd19ae55fb76f9838a71d65
dannytaylor <daniel@mclegoman.com>
2025-03-31 16:42:56 +1300

    ores

--- src/main/java/com/mclegoman/dtaf2025/common/world/feature/FeatureRegistry.java
+++ src/main/java/com/mclegoman/dtaf2025/common/world/feature/FeatureRegistry.java
@@ -10,14 +10,24 @@ package com.mclegoman.dtaf2025.common.world.feature;
 import com.mclegoman.dtaf2025.common.data.Data;
 import com.mclegoman.dtaf2025.common.world.feature.crater.CraterFeature;
 import com.mclegoman.dtaf2025.common.world.feature.crater.CraterFeatureConfig;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.PlacedFeature;
 
 public class FeatureRegistry {
+	public static final RegistryKey<PlacedFeature> oreLead = RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(Data.getVersion().getID(), "ore_lead"));
+	public static final RegistryKey<PlacedFeature> oreLeadSmall = RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(Data.getVersion().getID(), "ore_lead_small"));
 	public static final Feature<CraterFeatureConfig> crater;
 	public static void init() {
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreLead);
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreLeadSmall);
 	}
 	static {
 		crater = Registry.register(Registries.FEATURE, Identifier.of(Data.getVersion().getID(), "crater"), new CraterFeature(CraterFeatureConfig.codec));
